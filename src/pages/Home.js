c4bae6ea0bb13905b9690e6fb8e3bf2155a90768
Rotimi Jude <excel.rotimi@gmail.com>
2024-06-29 05:02:44 -0400

    added new codes to fetch data, JSON server

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
+import { useFetch } from "../hooks/useFetch"
+import './Home.css'
+
+
 export default function Home() {
+  const {data:articles, isPending, error} = useFetch('http://localhost:3000/articles')
   return (
-    <div>
-        <h2>Home Page</h2>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec consectetur nisi, quis mattis massa
-    </p>
+    <div className="home">
+        <h2>Articles</h2>
+        {isPending && <div>Loading....</div>}
+        {error && <div>{error}</div>}
+        {articles && articles.map ((article)=> (
+          <div key={article.id} className="card">
+            <h3>{article.title}</h3>
+            <p>By: {article.author}</p>
+          </div>
+
+        ))}
+    
     </div>
     
   )
